b2cfede79069e11c10dc8e5d924ced6843daff28
keane <blubworks.keane@gmail.com>
2025-03-29 18:21:59 +0800

    Add files via upload

---
+++ welcome prompt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << " _       __          __                                " << endl;
+    cout << "| |     / /  ___    / /  _____  ____    ____ ___   ___ " << endl;
+    cout << "| | /| / /  / _ \\  / /  / ___/ / __ \\  / __ `__ \\ / _ \\" << endl;
+    cout << "| |/ |/ /  /  __/ / /  / /__  / /_/ / / / / / / //  __/" << endl;
+    cout << "|__/|__/   \\___/ /_/   \\___/  \\____/ /_/ /_/ /_/ \\___/" << endl;
+
+    cout << " __  __                      " << endl;
+    cout << "  / / / /   _____  ___    _____" << endl;
+    cout << " / / / /   / ___/ / _ \\  / ___/" << endl;
+    cout << " /_/ / /   (__  ) /  __/ / /    " << endl;
+    cout << " \\____/   /____/  \\___/ /_/     " << endl;
+
+    cout << "Welcome Back!" << endl;
+    system("pause");
+}
